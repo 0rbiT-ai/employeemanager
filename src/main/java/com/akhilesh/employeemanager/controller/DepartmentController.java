45a8141b3deaeaafe2d6f588bec11ae2912c6602
Akhilesh <akhilesh6249@gmail.com>
2026-05-20 16:07:52 +0530

    Implement Employee and Department Controllers

--- src/main/java/com/akhilesh/employeemanager/controller/DepartmentController.java
+++ src/main/java/com/akhilesh/employeemanager/controller/DepartmentController.java
@@ -1,8 +1,13 @@
 package com.akhilesh.employeemanager.controller;
 
+import com.akhilesh.employeemanager.entities.Department;
 import com.akhilesh.employeemanager.services.DepartmentService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/departments")
@@ -11,5 +16,27 @@ public class DepartmentController {
 
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Department>> getAllDepartments(){
+        return new ResponseEntity<>(departmentService.getAllDepartments(),HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable UUID id){
+        return new ResponseEntity<>(departmentService.getDepartmentById(id),HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Department> addDepartment(@RequestBody Department department){
+        return new ResponseEntity<>(departmentService.addDepartment(department),HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Department> updateDepartmentById(@PathVariable UUID id, @RequestBody Department updatedDepartment){
+        return new ResponseEntity<>(departmentService.updateDepartmentById(id,updatedDepartment),HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteDepartmentById(@PathVariable UUID id){
+        departmentService.deleteDepartmentById(id);
+        return new ResponseEntity<>("Department Deleted Successfully.",HttpStatus.OK);
     }
 }
--- src/main/java/com/akhilesh/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/akhilesh/employeemanager/controller/EmployeeController.java
@@ -1,8 +1,13 @@
 package com.akhilesh.employeemanager.controller;
 
+import com.akhilesh.employeemanager.entities.Employee;
 import com.akhilesh.employeemanager.services.EmployeeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -11,5 +16,27 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        return new ResponseEntity<>(employeeService.getAllEmployees(),HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable UUID id){
+        return new ResponseEntity<>(employeeService.getEmployeeById(id),HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        return new ResponseEntity<>(employeeService.addEmployee(employee),HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployeeById(@PathVariable UUID id, @RequestBody Employee updatedEmployee){
+        return new ResponseEntity<>(employeeService.updateEmployeeById(id,updatedEmployee),HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployeeById(@PathVariable UUID id){
+        employeeService.deleteEmployeeById(id);
+        return new ResponseEntity<>("Employee Deleted Successfully.", HttpStatus.OK);
     }
 }
